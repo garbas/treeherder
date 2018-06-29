91201c6f249269366cb4646a42e6a97a279de44a
Cameron Dawson <cdawson@mozilla.com>
2018-06-28 17:32:06 -0700

    Bug 1472047 - Convert the Treeherder Update bar to ReactJS

--- ui/job-view/JobView.jsx
+++ ui/job-view/JobView.jsx
@@ -6,11 +6,15 @@ import { createBrowserHistory } from 'history';
 
 import treeherder from '../js/treeherder';
 import { thEvents } from '../js/constants';
+import { getRevisionTxtUrl } from '../helpers/url';
 import DetailsPanel from './details/DetailsPanel';
-import ActiveFilters from './navbars/ActiveFilters';
+import ActiveFilters from './headerbars/ActiveFilters';
+import UpdateAvailable from './headerbars/UpdateAvailable';
 import PushList from './PushList';
 
 const DEFAULT_DETAILS_PCT = 40;
+const REVISION_POLL_INTERVAL = 1000 * 60 * 5;
+const REVISION_POLL_DELAYED_INTERVAL = 1000 * 60 * 60;
 
 const getWindowHeight = function () {
   const windowHeight = window.innerHeight;
@@ -55,6 +59,7 @@ class JobView extends React.Component {
        ...this.thJobFilters.getFieldFiltersArray(),
       ],
       pushListPct: props.selectedJob ? 100 - DEFAULT_DETAILS_PCT : 100,
+      serverChangedDelayed: false,
     };
   }
 
@@ -76,6 +81,40 @@ class JobView extends React.Component {
         ],
       });
     });
+
+    // Only set up the revision polling interval if revision.txt exists on page load
+    this.checkServerRevision().then((revision) => {
+      this.setState({ serverRev: revision });
+      this.updateInterval = setInterval(() => {
+        this.checkServerRevision().then((revision) => {
+            const { serverChangedTimestamp, serverRev } = this.state;
+            if (this.$rootScope.serverChanged) {
+              if (Date.now() - serverChangedTimestamp > REVISION_POLL_DELAYED_INTERVAL) {
+                this.setState({ serverChangedDelayed: true });
+                // Now that we know there's an update, stop polling.
+                clearInterval(this.updateInterval);
+              }
+            }
+            // This request returns the treeherder git revision running on the server
+            // If this differs from the version chosen during the UI page load, show a warning
+            // Update $rootScope.serverRev so this warning is only shown once per server-side change
+            if (serverRev && serverRev !== revision) {
+              this.setState({ serverRev: revision });
+              if (this.$rootScope.serverChanged === false) {
+                this.$rootScope.serverChanged = true;
+                this.$rootScope.$apply();
+                this.setState({ serverChangedTimestamp: Date.now() });
+              }
+            }
+          });
+      }, REVISION_POLL_INTERVAL);
+    });
+  }
+
+  updateButtonClick() {
+    if (window.confirm('Reload the page to pick up Treeherder updates?')) {
+      window.location.reload(true);
+    }
   }
 
   toggleFieldFilterVisible() {
@@ -92,6 +131,15 @@ class JobView extends React.Component {
     });
   }
 
+  checkServerRevision() {
+    return fetch(getRevisionTxtUrl()).then((resp) => {
+      if (!resp.ok) {
+        throw Error(`Error loading revision.txt: ${resp.statusText}`);
+      }
+      return resp.text();
+    });
+  }
+
   render() {
     const {
       user, repoName, revision, currentRepo, selectedJob, $injector,
@@ -99,6 +147,7 @@ class JobView extends React.Component {
     const {
       isFieldFilterVisible, filterBarFilters,
       defaultPushListPct, defaultDetailsHeight, latestSplitPct,
+      serverChangedDelayed,
     } = this.state;
     // SplitPane will adjust the CSS height of the top component, but not the
     // bottom component.  So the scrollbars won't work in the DetailsPanel when
@@ -125,6 +174,9 @@ class JobView extends React.Component {
             history={this.history}
             isFieldFilterVisible={isFieldFilterVisible}
             toggleFieldFilterVisible={this.toggleFieldFilterVisible}
+          />}
+          {serverChangedDelayed && <UpdateAvailable
+            updateButtonClick={this.updateButtonClick}
           />}
           <div id="th-global-content" className="th-global-content">
             <span className="th-view-content">
